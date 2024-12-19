6ba4fec73aa6794c187e614cdf1a620c2dee22c9
Ri10yaa <rithanyaame@gmail.com>
2024-12-19 14:02:00 +0530

    Initial commit

---
+++ src/main/java/com/project/restaurantOrderingManagement/RestaurantOrderingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.restaurantOrderingManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOrderingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantOrderingManagementApplication.class, args);
+	}
+
+}
