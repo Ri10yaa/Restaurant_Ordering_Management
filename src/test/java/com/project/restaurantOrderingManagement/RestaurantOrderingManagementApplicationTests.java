e2f8ccc1074cab90f4104dc9011979e0fc59bc89
Ri10yaa <rithanyaame@gmail.com>
2024-12-19 14:02:00 +0530

    Initial commit

---
+++ src/test/java/com/project/restaurantOrderingManagement/RestaurantOrderingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.restaurantOrderingManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOrderingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
